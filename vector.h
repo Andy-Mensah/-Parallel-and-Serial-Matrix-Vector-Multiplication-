f9069dba6d1d306bee0919f1b910ec5566933f9c
Andy-Mensah <andrewsteimensah@gmail.com>
2024-11-06 14:52:06 +0000

    Delete vector.h

--- vector.h
+++
@@ -1,2 +0,0 @@
-double vector_vector( int m, int n , double * vecA , double * vecB );
-double runtime_vec_vec(double (*function)(int, int , double *, double *), int m, int n, double * vecA , double * vecB );
